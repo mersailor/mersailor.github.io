9f60f11d78f8da09fc19a865a1e660ef976819cd
Sibylle Eid <sib.weber@gmail.com>
2022-07-03 06:58:18 +0200

    Added Prism plugin for eleventy

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,10 @@
+// Plugin for PrismJS syntax highlighting
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.markdownTemplateEngine = "njk";
+  eleventyConfig.addPlugin(syntaxHighlight);
 
   return {
     dir: {
